5d1e278601d40e6e6841156c4a01fc20608df7f5
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-11-28 10:37:52 +0000

    router
    co-authored by Dan <dangoss@posteo.net>

--- src/router.js
+++ src/router.js
@@ -1,1 +1,21 @@
 const handlers = require("./handlers");
+
+const router = (request, response) => {
+const endpoint = request.url;
+
+if (endpoint === "/") {
+    handlers.handleHome(request, response);
+} 
+else if (endpoint.includes("public")) {
+    handlers.handlePublic(request, response, endpoint); 
+} 
+else if (endpoint.includes("search")) {
+    handlers.handleData(request, response, endpoint);
+} else {
+    response.writeHead(404); 
+    response.end("404 not found")
+}
+
+}
+
+module.exports = router; 
