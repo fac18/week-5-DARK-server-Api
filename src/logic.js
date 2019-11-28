674565b4637e745b5f9ee967a9e4dc1b1dc4c785
Kin Au <kinnamau@gmail.com>
2019-11-28 12:40:34 +0000

    return decision as object
    co-authored-by: ayub3 <a.mohamud1@hotmail.com>

--- src/logic.js
+++ src/logic.js
@@ -1,40 +1,39 @@
 let decision = {};
 
 const goOutDecision = (weatherCode, sunrise, sunset) => {
-    // if current time is before sunrise, or after sunset, then return decision no
-    const date = new Date();
-    let timeStamp = Math.floor(date.getTime()/1000);
+    // If current time is before sunrise, or after sunset, then return decision no
+    let timeStamp = Math.floor(new Date().getTime()/1000);
     if ((timeStamp < sunrise) || (sunset < timeStamp)) {
         decision.yesno = "No";
         decision.reason = "It's dark and scary outside";
     } else {
     // If current time is after sunrise and before sunset, then check weather
         if (weatherCode.startsWith("2")) {
-            // thunderstorm weather code
+            // Thunderstorm weather code
             decision.yesno = "No";
             decision.reason = "You don't want to get struck by lightning";
         } else if (weatherCode.startsWith("3")) {
-            // drizzle weather code
+            // Drizzle weather code
             decision.yesno = "Maybe";
             decision.reason = "There's a little drizzle";
         } else if (weatherCode.startsWith("5")) {
-            // rain weather code
+            // Rain weather code
             decision.yesno = "No";
             decision.reason = "It's raining outside";
         } else if (weatherCode.startsWith("6")) {
-            // snow weather code
+            // Snow weather code
             decision.yesno = "Maybe";
             decision.reason = "Do you want to build a snowman?";
         } else if (weatherCode.startsWith("7")) {
-            // atmosphere weather code
+            // Atmosphere weather code
             decision.yesno = "No";
             decision.reason = "It's all bad";
         } else if (weatherCode === "800") {
-            // clear sky weather code
+            // Clear sky weather code
             decision.yesno = "Yes";
             decision.reason = "It's nice out there!";
         } else if (weatherCode.startsWith("8")) {
-            // cloudy weather code
+            // Cloudy weather code
             decision.yesno = "Yes";
             decision.reason = "Cloudy with a chance of meatballs";
         }
