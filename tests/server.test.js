6e3c194240aff7d2fcae1a627396b80e61b9b247
Rosalie-Baxter <rosalie.baxter@outlook.com>
2019-11-28 17:16:34 +0000

    checked for 404s on server
    co-authored by ayub <a.mohamud1@hotmail.com>

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,20 +4,38 @@ const test = require('tape');
 const supertest = require('supertest');
 const router = require("../src/router");
 
-test('Initialise', (t) => {
-    let num = 2
-    t.equal(num, 2, 'Should return 2');
-    t.end(); // Remember to call t.end() after every test call, to ensure tests run in order. You can also investigate t.plan() in the docs
+  test("Home route returns a status code of 200", (t) => {
+      supertest(router)
+      .get("/")
+      .expect(200)
+      .expect("content-type", /html/)
+      .end((err, res) => {
+          t.error(err);
+          t.equal(res.statusCode, 200, "response should contain 200 status code"); 
+          t.end();
+      })
   })
 
-//   test("check status code is 200", (t) => {
-//       supertest(router)
-//       .get("/")
-//       .expect(200)
-//       .expect("content-type", /html/)
-//       .end((err, res) => {
-//           t.error(error);
-//           t.equal(res.text, "Hello", "response should contain \'Hello\'"); 
-//           t.end();
-//       })
-//   })
+  test("Home route returns a status code of 404", (t) => {
+    supertest(router)
+    .get("/fhwefbewskjbf")
+    .expect(404)
+    .expect("content-type", /html/)
+    .end((err, res) => {
+        t.error(err);
+        t.equal(res.statusCode, 404, "response should contain 404 status code"); 
+        t.end();
+    })
+})
+
+// test("Home route returns a status code of 404", (t) => {
+//     supertest(router)
+//     .get("/fhwefbewskjbf")
+//     .expect(404)
+//     .expect("content-type", /html/)
+//     .end((err, res) => {
+//         t.error(err);
+//         t.equal(res.statusCode, 404, "response should contain 404 status code"); 
+//         t.end();
+//     })
+// })
