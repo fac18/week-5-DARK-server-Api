ab1f346d26f85f5a71af7bc59c2c7a1674c74545
plasticneuron <plasticneuron@posteo.net>
2019-11-28 12:46:30 +0000

    merge with master and prettify everything
    co-authored-by: Rosa <rosalie.baxter@outlook.com>

--- src/logic.js
+++ src/logic.js
@@ -1,44 +1,44 @@
 let decision = {};
 
 const goOutDecision = (weatherCode, sunrise, sunset) => {
-    // If current time is before sunrise, or after sunset, then return decision no
-    let timeStamp = Math.floor(new Date().getTime()/1000);
-    if ((timeStamp < sunrise) || (sunset < timeStamp)) {
-        decision.yesno = "No";
-        decision.reason = "It's dark and scary outside";
-    } else {
+  // If current time is before sunrise, or after sunset, then return decision no
+  let timeStamp = Math.floor(new Date().getTime() / 1000);
+  if (timeStamp < sunrise || sunset < timeStamp) {
+    decision.yesno = "No";
+    decision.reason = "It's dark and scary outside";
+  } else {
     // If current time is after sunrise and before sunset, then check weather
-        if (weatherCode.startsWith("2")) {
-            // Thunderstorm weather code
-            decision.yesno = "No";
-            decision.reason = "You don't want to get struck by lightning";
-        } else if (weatherCode.startsWith("3")) {
-            // Drizzle weather code
-            decision.yesno = "Maybe";
-            decision.reason = "There's a little drizzle";
-        } else if (weatherCode.startsWith("5")) {
-            // Rain weather code
-            decision.yesno = "No";
-            decision.reason = "It's raining outside";
-        } else if (weatherCode.startsWith("6")) {
-            // Snow weather code
-            decision.yesno = "Maybe";
-            decision.reason = "Do you want to build a snowman?";
-        } else if (weatherCode.startsWith("7")) {
-            // Atmosphere weather code
-            decision.yesno = "No";
-            decision.reason = "It's all bad";
-        } else if (weatherCode === "800") {
-            // Clear sky weather code
-            decision.yesno = "Yes";
-            decision.reason = "It's nice out there!";
-        } else if (weatherCode.startsWith("8")) {
-            // Cloudy weather code
-            decision.yesno = "Yes";
-            decision.reason = "Cloudy with a chance of meatballs";
-        }
+    if (weatherCode.startsWith("2")) {
+      // Thunderstorm weather code
+      decision.yesno = "No";
+      decision.reason = "You don't want to get struck by lightning";
+    } else if (weatherCode.startsWith("3")) {
+      // Drizzle weather code
+      decision.yesno = "Maybe";
+      decision.reason = "There's a little drizzle";
+    } else if (weatherCode.startsWith("5")) {
+      // Rain weather code
+      decision.yesno = "No";
+      decision.reason = "It's raining outside";
+    } else if (weatherCode.startsWith("6")) {
+      // Snow weather code
+      decision.yesno = "Maybe";
+      decision.reason = "Do you want to build a snowman?";
+    } else if (weatherCode.startsWith("7")) {
+      // Atmosphere weather code
+      decision.yesno = "No";
+      decision.reason = "It's all bad";
+    } else if (weatherCode === "800") {
+      // Clear sky weather code
+      decision.yesno = "Yes";
+      decision.reason = "It's nice out there!";
+    } else if (weatherCode.startsWith("8")) {
+      // Cloudy weather code
+      decision.yesno = "Yes";
+      decision.reason = "Cloudy with a chance of meatballs";
     }
-    return decision;
-}
+  }
+  return decision;
+};
 
-module.exports = { goOutDecision }
+module.exports = { goOutDecision };
