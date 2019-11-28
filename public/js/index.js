0f9e5a50bd57dfe46a7ffd5f969e2d20720d7021
Ayub Mohamud <a.mohamud1@hotmail.com>
2019-11-28 10:51:55 +0000

    added some frontend

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,21 @@
-// index.js
+const cities = document.querySelector('#location');
+const searchButton = document.querySelector('#search-button')
+
+const searchLocation = (event) => {
+event.preventDefault();
+    const xhr = new XMLHttpRequest();
+    const url = "?cities=" + cities.value;
+    console.log(url);
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log(xhr)
+            // const response = JSON.parse(xhr.responseText);
+            // do something with response
+        }
+    }
+    xhr.open("GET", url, true);
+    xhr.send();
+}
+
+searchButton.addEventListener('click', searchLocation) 
+
