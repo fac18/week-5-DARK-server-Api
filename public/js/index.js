cd480fb68b43213e5e3984453904ba1a072a1604
plasticneuron <plasticneuron@posteo.net>
2019-11-28 11:34:38 +0000

    Write handlers for home and public files
    co-authored-by: Rosa <rosalie.baxter@outlook.com>

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,20 @@
-const cities = document.querySelector('#location');
-const searchButton = document.querySelector('#search-button')
+const cities = document.querySelector("#location");
+const searchButton = document.querySelector("#search-button");
 
-const searchLocation = (event) => {
-event.preventDefault();
-    const xhr = new XMLHttpRequest();
-    const url = "?cities=" + cities.value;
-    console.log(url);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4 && xhr.status === 200) {
-            console.log(xhr)
-            // const response = JSON.parse(xhr.responseText);
-            // do something with response
-        }
+const searchLocation = event => {
+  event.preventDefault();
+  const xhr = new XMLHttpRequest();
+  const url = "/search?cities=" + cities.value;
+  console.log(url);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log(xhr);
+      // const response = JSON.parse(xhr.responseText);
+      // do something with response
     }
-    xhr.open("GET", url, true);
-    xhr.send();
-}
+  };
+  xhr.open("GET", url, true);
+  xhr.send();
+};
 
-searchButton.addEventListener('click', searchLocation) 
-
+searchButton.addEventListener("click", searchLocation);
--- src/handlers.js
+++ src/handlers.js
@@ -1,3 +1,53 @@
 const path = require("path");
 const fs = require("fs");
 const api = require("./api");
+
+const handleHome = (request, response) => {
+  const filePath = path.join(__dirname, "../public/index.html");
+  fs.readFile(filePath, (err, file) => {
+    if (err) {
+      console.log(err);
+      response.writeHead(500, { "content-type": "text/html" });
+      response.end("A problem has occurred on our end - sorry folks!");
+    } else {
+      response.writeHead(200, { "content-type": "text/html" });
+      response.end(file);
+    }
+  });
+};
+
+const handlePublic = (request, response, endpoint) => {
+  const extension = endpoint.split(".")[1];
+  const extensionType = {
+    html: "text/html",
+    css: "text/css",
+    js: "application/js",
+    ico: "image/x-icon",
+    svg: "image/svg+xml",
+    jpeg: "image/jpeg"
+  };
+  const filePath = path.join(__dirname, "..", endpoint);
+  fs.readFile(filePath, (error, file) => {
+    if (error) {
+      console.log(error);
+      fs.readFile(
+        path.join(__dirname, "..", "/public/not-found.html"),
+        (err2, file2) => {
+          if (err2) {
+            console.log(err2);
+          } else {
+            response.writeHead(404, { "content-type": "text/html" });
+            response.end(file2);
+          }
+        }
+      );
+    } else {
+      response.writeHead(200, { "content-type": extensionType[extension] });
+      response.end(file);
+    }
+  });
+};
+
+const handleData = (request, response, endpoint) => {};
+
+module.exports = { handleHome, handlePublic };
